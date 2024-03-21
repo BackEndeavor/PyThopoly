586f880a36f28be2ab08fec53da554e9e05afff4
bivashy <85439143+bivashy@users.noreply.github.com>
2024-03-22 03:15:06 +0500

    Replace increment with Player#next_step(amount) (#7)

--- src/entity/player.py
+++ src/entity/player.py
@@ -11,3 +11,10 @@ class Player:
     def draw(self):
         x, y = self.board.index_to_position(self.position_index)
         arcade.draw_circle_filled(x, y, self.radius, arcade.color.GOLD)
+
+    def next_step(self, amount):
+        new_position = self.position_index + amount
+        if new_position not in self.board.positions:
+            self.position_index = new_position % len(self.board.positions)
+            return
+        self.position_index += amount
